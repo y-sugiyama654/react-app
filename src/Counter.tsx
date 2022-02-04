877f6f686ca150df27fefe2f5c161a1f4c658ec3
Yuta <y.sugiyama54@gmail.com>
2022-02-05 00:53:40 +0900

    Optional ChainingとNon-Null Assertion Operator

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -16,12 +16,19 @@ const Counter: React.FC<{}> = () => {
         renderTimes.current = renderTimes.current + 1;
     });
 
+    const ref = useRef<HTMLInputElement>(null!);
+    const focusInput = () => {
+        ref.current.focus();
+    }
+
     return (
         <div>
             <div>value: { value }</div>
             <button onClick={increment}>+1</button>
             <button onClick={decremrnt}>-1</button>
             <div>This component was re-rendered { renderTimes.current } times!</div>
+            <input ref={ref} type="text" />
+            <button onClick={focusInput}>Click Me!</button>
         </div>
     );
 }
